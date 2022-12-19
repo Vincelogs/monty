f4e3dab88a74c6c1fe299dc4d3ce195dd22d7882
George Osondu <browngeorge752@gmail.com>
2022-12-19 02:08:55 +0000

    stack

--- m_stack.c
+++ m_stack.c
@@ -5,7 +5,7 @@
  * @stack: double pointer to the beginning of the stack
  * @line_number: script line number
  *
- * Return: void
+ * Return: nothing
  */
 void m_stack(stack_t **stack, unsigned int line_number)
 {
